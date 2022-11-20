a6bfe26f33f50921eed71bf4eb5504a23709eaa4
Cédric Carré <cedric.carre@arolla.fr>
2022-11-20 15:33:25 +0100

    override equals() hashCode toString consistent with an object value

--- src/main/java/fr/chaplinB/Amount.java
+++ src/main/java/fr/chaplinB/Amount.java
@@ -1,6 +1,7 @@
 package fr.chaplinB;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public final class Amount {
     private final BigDecimal amount;
@@ -11,5 +12,25 @@ public final class Amount {
 
     public Amount plus(Amount anotherAmount) {
         return new Amount(amount.add(anotherAmount.amount));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Amount that = (Amount) o;
+        return amount.equals(that.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
+
+    @Override
+    public String toString() {
+        return "Amount{" +
+                "amount=" + amount +
+                '}';
     }
 }
