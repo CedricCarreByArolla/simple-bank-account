131af804850422468d0cd85838d0bb16a5acb5d0
Cédric Carré <cedric.carre@arolla.fr>
2022-11-20 19:45:45 +0100

    complete implementation of ClockTicker class

--- src/test/java/fr/chaplinB/ClockTicker.java
+++ src/test/java/fr/chaplinB/ClockTicker.java
@@ -4,8 +4,14 @@ import java.time.Clock;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 
 public class ClockTicker extends Clock {
+    private final Instant ACCOUNT_CREATION_DATE_ON_2022_12_10 = LocalDate.of(2022,12,10)
+            .atStartOfDay(ZoneId.of("Europe/Paris"))
+            .toInstant();
+    private int count = 0;
+
     @Override
     public ZoneId getZone() {
         return ZoneId.of("Europe/Paris");
@@ -14,15 +20,18 @@ public class ClockTicker extends Clock {
     @Override
     public Clock withZone(ZoneId zone) {
         return Clock.fixed(
-                LocalDate.of(2022,12,10)
-                        .atStartOfDay(ZoneId.of("Europe/Paris"))
-                        .toInstant(),
+                ACCOUNT_CREATION_DATE_ON_2022_12_10,
                 zone
         );
     }
 
     @Override
     public Instant instant() {
-        return null;
+        return nextInstant();
+    }
+
+    private Instant nextInstant() {
+        ++count;
+        return ACCOUNT_CREATION_DATE_ON_2022_12_10.plus(count, ChronoUnit.DAYS);
     }
 }
