4eb99ac81bc98fa9b772c1a501e3208f94589591
Cédric Carré <cedric.carre@arolla.fr>
2022-11-20 15:03:21 +0100

    test: check a deposit of 500 on bank account with balance of 1500

--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BankAccountTest {
@@ -7,5 +9,25 @@ public class BankAccountTest {
     @Test
     void should_be_always_true() {
         assertThat(true).isTrue();
+    }
+
+    @Test
+    void
+    given_an_new_account_with_an_initial_deposit_of_an_amount_of_1500_when_we_deposit_500_should_return_a_balance_of_2000() {
+        //Arrange
+        BigDecimal initialAmountAsBigDecimal = new BigDecimal("1500.00");
+        Amount initialAmountToDeposit = new Amount(initialAmountAsBigDecimal);
+
+        BankAccount bankAccount = new BankAccount(Amount initialAmountToDeposit);
+
+        BigDecimal amountToDepositAsBigDecimal = new BigDecimal("500.00");
+        Amount amountToDeposit = new Amount(amountToDepositAsBigDecimal);
+
+        BigDecimal expectedAsBigDecimal = new BigDecimal("2000.00");
+        Amount expectedBalance = new Amount(expectedAsBigDecimal);
+        //Act
+        bankAccount.deposit(amountToDeposit);
+        //Assert
+        assertThat(bankAccount.getBalance()).isEqualTo(expectedBalance);
     }
 }
