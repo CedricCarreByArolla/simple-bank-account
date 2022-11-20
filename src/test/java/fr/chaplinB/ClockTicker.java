3cc1f420dd537c4e35faf903c0f6448b7127ad86
Cédric Carré <cedric.carre@arolla.fr>
2022-11-20 20:01:40 +0100

    refactor: extrat constante , all tests passed

--- src/test/java/fr/chaplinB/ClockTicker.java
+++ src/test/java/fr/chaplinB/ClockTicker.java
@@ -13,14 +13,15 @@ import java.time.temporal.ChronoUnit;
 * Do not use this class in production code.
 * */
 public class ClockTicker extends Clock {
+    public static final ZoneId PARIS_ZONE_ID = ZoneId.of("Europe/Paris");
     private final Instant ACCOUNT_CREATION_DATE_ON_2022_12_09 = LocalDate.of(2022,12,9)
-            .atStartOfDay(ZoneId.of("Europe/Paris"))
+            .atStartOfDay(PARIS_ZONE_ID)
             .toInstant();
     private int count = 0;
 
     @Override
     public ZoneId getZone() {
-        return ZoneId.of("Europe/Paris");
+        return PARIS_ZONE_ID;
     }
 
     @Override
