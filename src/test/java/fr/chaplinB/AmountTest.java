59747d60af276232e7ac5cb0eeac67a061cd1248
Cédric Carré <cedric.carre@arolla.fr>
2022-11-20 15:55:20 +0100

    test: an Amount should never be negative

--- src/test/java/fr/chaplinB/AmountTest.java
+++ src/test/java/fr/chaplinB/AmountTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
 class AmountTest {
     @Test
@@ -16,5 +17,16 @@ class AmountTest {
         Amount anotherAmount = Amount.valueOf(amountAsBig);
         //Assert
         assertThat(amount).isEqualTo(anotherAmount);
+    }
+
+    @Test
+    void should_always_be_positive_number() {
+        //Arrange
+        BigDecimal negativeNumber = new BigDecimal("-100.00");
+        Throwable throwable = new IllegalAmountException("amount must be positive number !");
+        //Act
+        Throwable thrown = catchThrowable(() -> Amount.valueOf(negativeNumber));
+        //Assert
+        assertThat(thrown).isInstanceOf(IllegalAmountException.class);
     }
 }
