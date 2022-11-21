cf9aefdea66d250df94688a0c6a9e0933223acef
Cédric Carré <cedric.carre@arolla.fr>
2022-11-21 10:36:04 +0100

    make getall() in AccountStatement return unmodifialbleList

--- src/main/java/fr/chaplinB/AccountStatement.java
+++ src/main/java/fr/chaplinB/AccountStatement.java
@@ -1,13 +1,14 @@
 package fr.chaplinB;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class AccountStatement {
     List<Operation> operations = new ArrayList<>();
 
     public List<Operation> getAll() {
-        return operations;
+        return Collections.unmodifiableList(operations);
     }
 
     public void add(Operation operation) {
