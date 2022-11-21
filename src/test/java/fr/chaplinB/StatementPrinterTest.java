ea4bd58cf96ddf5739f8cc2dd9f87e65a5675062
Cédric Carré <cedric.carre@arolla.fr>
2022-11-21 11:39:25 +0100

    test: check header of account statement print and initial operation print

--- src/test/java/fr/chaplinB/StatementPrinterTest.java
+++ src/test/java/fr/chaplinB/StatementPrinterTest.java
@@ -2,9 +2,13 @@ package fr.chaplinB;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class StatementPrinterTest {
 
@@ -14,6 +18,21 @@ public class StatementPrinterTest {
     @BeforeEach
     public void setUp() {
         System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @Test
+    void should_print_account_statement_header_and_initial_operation() {
+        //Arrange
+        ClockTicker clockTicker = new ClockTicker();
+        Amount initialAmount = Amount.valueOf(new BigDecimal("200"));
+        AccountStatement accountStatement = new AccountStatement();
+        BankAccount bankAccount = new BankAccount(initialAmount, accountStatement, clockTicker);
+        //Act
+        bankAccount.printStatement();
+        //Assert
+        String expectedResult = "Operation | Date | Amount\n" +
+                                "DEPOSIT | 2022-12-10 | 200";
+        assertThat(outputStreamCaptor.toString().trim()).isEqualTo(expectedResult);
     }
 
     @AfterEach
