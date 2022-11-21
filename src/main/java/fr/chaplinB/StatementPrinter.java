fb723624f99b0a1db582139c9a3339502ce409fd
Cédric Carré <cedric.carre@arolla.fr>
2022-11-21 11:48:04 +0100

    implement print() in StatemenetPrinter, test still fails

--- src/main/java/fr/chaplinB/StatementPrinter.java
+++ src/main/java/fr/chaplinB/StatementPrinter.java
@@ -3,7 +3,16 @@ package fr.chaplinB;
 import java.util.List;
 
 public class StatementPrinter {
-    public void print(List<Operation> accountStatement) {
-        throw new UnsupportedOperationException();
+    public void print(List<Operation> allOperations) {
+        System.out.println("Operation | Date | Amount");
+        allOperations.forEach(
+                operation -> {
+                    System.out.println(operation.operationType().toString() +
+                            " | " +
+                            operation.localDate().toString() +
+                            " | " +
+                            operation.amount().toString());
+                }
+        );
     }
 }
