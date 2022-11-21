a7aafaca319549320bdb62b61e927b7a2e6bc59e
Cédric Carré <cedric.carre@arolla.fr>
2022-11-21 13:01:47 +0100

    test: final test to show after multiple operation on different dates

--- src/test/java/fr/chaplinB/StatementPrinterTest.java
+++ src/test/java/fr/chaplinB/StatementPrinterTest.java
@@ -43,6 +43,27 @@ class StatementPrinterTest {
         assertThat(outputStreamCaptor.toString().trim()).isEqualTo(expectedResult);
     }
 
+    @Test
+    void should_print_account_statement_consistent_with_my_operations() {
+        //Arrange
+        BigDecimal amountToDepositAsBigDecimal = new BigDecimal("1500.00");
+        Amount amountToDeposit = Amount.valueOf(amountToDepositAsBigDecimal);
+        BigDecimal amountToWithdrawAsBigDecimal = new BigDecimal("1000.00");
+        Amount amountToWithdraw = Amount.valueOf(amountToWithdrawAsBigDecimal);
+        //Act
+        bankAccount.deposit(amountToDeposit);
+        bankAccount.withdraw(amountToWithdraw);
+        bankAccount.printStatement();
+        //Assert
+        String expectedResult = """
+                Operation | Date | Amount\r
+                DEPOSIT | 2022-12-10 | 200.00\r
+                DEPOSIT | 2022-12-11 | 1500.00\r
+                WITHDRAWAL | 2022-12-12 | 1000.00\r
+                Your balance : 700.00""";
+        assertThat(outputStreamCaptor.toString().trim()).isEqualTo(expectedResult);
+    }
+
     @AfterEach
     public void tearDown() {
         System.setOut(standardOut);
